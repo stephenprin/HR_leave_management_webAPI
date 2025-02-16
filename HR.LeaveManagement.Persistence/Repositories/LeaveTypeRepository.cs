13a097036e978d2ba21c5d5b90103f2653278dbf
stephenprin <stephenprince427@gmail.com>
2025-02-15 16:18:12 -0800

    configure repository for different domain

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        public LeaveTypeRepository(HrDatabaseContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> IsLeaveTypeNameUnique(string name)
+        {
+            return await _context.LeaveTypes.AnyAsync(t => t.Name == name);
+        }
+    }
+}
